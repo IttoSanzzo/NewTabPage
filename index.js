f613f2fb97e751adaaf9e99c4609deeb44c98aec
IttoSanzzo <konohittos@gmail.com>
2024-12-20 13:54:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const BackgroundImage = document.getElementsByClassName("Image fullscreen")[0];
+const clockTime = document.getElementById("clockTime");
+const dateString = document.getElementById("dateString");
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function updateBackgroundPosition() {
+	BackgroundImage.style.backgroundPosition = `${-window.screenLeft - 5}px ${-window.screenTop}px`;
+}
+function updateClock() {
+	const nowTime = new Date();
+	clockTime.textContent = `${String(nowTime.getHours()).padStart(2, "0")}:${String(nowTime.getMinutes()).padStart(2, "0")} `
+}
+function updateDateString() {
+	const nowTime = new Date();
+	dateString.textContent = `${daysOfWeek[nowTime.getDay()]}, ${months[nowTime.getMonth()]} ${nowTime.getDate()}`;
+}
+window.addEventListener("resize", updateBackgroundPosition);
+window.addEventListener("load", updateBackgroundPosition);
+setInterval(updateBackgroundPosition, 5000);
+setInterval(updateClock, 5000);
+setInterval(updateDateString, 60000);
+updateClock();
+updateDateString();
