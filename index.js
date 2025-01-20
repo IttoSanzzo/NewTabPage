83d5a2c48f2e2fd0445a6ae36120a4875a9aec18
IttoSanzzo <konohittos@gmail.com>
2025-01-20 10:44:38 -0300

    Multi Screen Fix

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const clockTime = document.getElementById("clockTime");
 const dateString = document.getElementById("dateString");
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 function updateBackgroundPosition() {
-	BackgroundImage.style.backgroundPosition = `${-window.screenLeft - 5}px ${-window.screenTop}px`;
+	if ((window.screenLeft < -5 || window.screenTop < -5) || (window.screenLeft > 3435)) {
+		BackgroundImage.style.backgroundPosition = "center";
+		BackgroundImage.style.backgroundSize = "cover";
+	}
+	else {
+		BackgroundImage.style.backgroundPosition = `${-window.screenLeft - 5}px ${-window.screenTop}px`;
+		BackgroundImage.style.backgroundSize = "auto";
+	}
 }
 function updateClock() {
 	const nowTime = new Date();
@@ -14,6 +22,7 @@ function updateDateString() {
 	const nowTime = new Date();
 	dateString.textContent = `${daysOfWeek[nowTime.getDay()]}, ${months[nowTime.getMonth()]} ${nowTime.getDate()}`;
 }
+
 window.addEventListener("resize", updateBackgroundPosition);
 window.addEventListener("load", updateBackgroundPosition);
 setInterval(updateBackgroundPosition, 5000);
